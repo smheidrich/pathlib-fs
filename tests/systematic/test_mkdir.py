cca55b103394239df3dea5d32eca15e86ffe5364
smheidrich <smheidrich@weltenfunktion.de>
2019-05-13 01:40:31 +0200

    Simplify systematic tests

--- tests/systematic/test_mkdir.py
+++ tests/systematic/test_mkdir.py
@@ -5,31 +5,28 @@ from pathlib import Path
 
 import pytest
 
-def create_some_dir_in_tmpdir(general_fs_tmpdir):
+def script(check, general_fs_tmpdir):
   p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_dir")
   p.mkdir()
-  yield
+  check()
   with pytest.raises(Exception):
     p.mkdir()
   p.mkdir(exist_ok=True)
 
 def test_check_with_pyfilesystem(general_fs_tmpdir):
-  g = create_some_dir_in_tmpdir(general_fs_tmpdir)
-  next(g)
-  path = fs.path.join(general_fs_tmpdir.path, "some_dir")
-  assert general_fs_tmpdir.fs.isdir(path)
-  next(g, None)
+  def check():
+    path = fs.path.join(general_fs_tmpdir.path, "some_dir")
+    assert general_fs_tmpdir.fs.isdir(path)
+  script(check, general_fs_tmpdir)
 
 def test_check_with_pathlib(general_osfs_tmpdir):
-  g = create_some_dir_in_tmpdir(general_osfs_tmpdir)
-  next(g)
-  path = Path(general_osfs_tmpdir.os_path)/"some_dir"
-  assert path.is_dir()
-  next(g, None)
+  def check():
+    path = Path(general_osfs_tmpdir.os_path)/"some_dir"
+    assert path.is_dir()
+  script(check, general_osfs_tmpdir)
 
 def test_check_self_consistency(general_fs_tmpdir):
-  g = create_some_dir_in_tmpdir(general_fs_tmpdir)
-  next(g)
-  p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_dir")
-  assert p.is_dir()
-  next(g, None)
+  def check():
+    p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_dir")
+    assert p.is_dir()
+  script(check, general_fs_tmpdir)
--- tests/systematic/test_open_w.py
+++ tests/systematic/test_open_w.py
@@ -1,33 +1,30 @@
 from pathlib_fs import FsPath
 
+from contextlib import contextmanager
 import fs.path
 from pathlib import Path
 
-def write_some_file_in_tmpdir(general_fs_tmpdir):
+def script(yield_f, general_fs_tmpdir):
   p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_file")
   with p.open("w") as f:
     f.write("hello")
-  f = yield
-  assert f.read() == "hello"
-  yield # dummy -> no StopIteration TODO
+  with yield_f() as f:
+    assert f.read() == "hello"
 
 def test_check_with_pyfilesystem(general_fs_tmpdir):
-  g = write_some_file_in_tmpdir(general_fs_tmpdir)
-  next(g)
-  path = fs.path.join(general_fs_tmpdir.path, "some_file")
-  with general_fs_tmpdir.fs.open(path) as f:
-    g.send(f)
+  def yield_f():
+    path = fs.path.join(general_fs_tmpdir.path, "some_file")
+    return general_fs_tmpdir.fs.open(path)
+  script(yield_f, general_fs_tmpdir)
 
 def test_check_with_pathlib(general_osfs_tmpdir):
-  g = write_some_file_in_tmpdir(general_osfs_tmpdir)
-  next(g)
-  path = Path(general_osfs_tmpdir.os_path)/"some_file"
-  with path.open() as f:
-    g.send(f)
+  def yield_f():
+    path = Path(general_osfs_tmpdir.os_path)/"some_file"
+    return path.open()
+  script(yield_f, general_osfs_tmpdir)
 
 def test_check_self_consistency(general_fs_tmpdir):
-  g = write_some_file_in_tmpdir(general_fs_tmpdir)
-  next(g)
-  p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_file")
-  with p.open() as f:
-    g.send(f)
+  def yield_f():
+    p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_file")
+    return p.open()
+  script(yield_f, general_fs_tmpdir)
