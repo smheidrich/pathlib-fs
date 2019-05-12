975f56649e004b48c6cd3f72a382679f3896c82a
smheidrich <smheidrich@weltenfunktion.de>
2019-05-12 21:16:09 +0200

    Rename systematic tests again

--- tests/systematic/test_mkdir.py
+++ tests/systematic/test_mkdir.py
@@ -12,17 +12,17 @@ def create_some_dir_in_tmpdir(general_fs_tmpdir):
     p.mkdir()
   p.mkdir(exist_ok=True)
 
-def check_with_pyfilesystem(general_fs_tmpdir):
+def test_check_with_pyfilesystem(general_fs_tmpdir):
   create_some_dir_in_tmpdir(general_fs_tmpdir)
   path = fs.path.join(general_fs_tmpdir.path, "some_dir")
   assert general_fs_tmpdir.fs.isdir(path)
 
-def check_with_pathlib(general_osfs_tmpdir):
+def test_check_with_pathlib(general_osfs_tmpdir):
   create_some_dir_in_tmpdir(general_osfs_tmpdir)
   path = Path(general_osfs_tmpdir.os_path)/"some_dir"
   assert path.is_dir()
 
-def check_self_consistency(general_fs_tmpdir):
+def test_check_self_consistency(general_fs_tmpdir):
   create_some_dir_in_tmpdir(general_fs_tmpdir)
   p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_dir")
   assert p.is_dir()
--- tests/systematic/test_open_w.py
+++ tests/systematic/test_open_w.py
@@ -11,19 +11,19 @@ def write_some_file_in_tmpdir(general_fs_tmpdir):
 def check_file_contents(f):
   assert f.read() == "hello"
 
-def check_with_pyfilesystem(general_fs_tmpdir):
+def test_check_with_pyfilesystem(general_fs_tmpdir):
   write_some_file_in_tmpdir(general_fs_tmpdir)
   path = fs.path.join(general_fs_tmpdir.path, "some_file")
   with general_fs_tmpdir.fs.open(path) as f:
     check_file_contents(f)
 
-def check_with_pathlib(general_osfs_tmpdir):
+def test_check_with_pathlib(general_osfs_tmpdir):
   write_some_file_in_tmpdir(general_osfs_tmpdir)
   path = Path(general_osfs_tmpdir.os_path)/"some_file"
   with path.open() as f:
     check_file_contents(f)
 
-def check_self_consistency(general_fs_tmpdir):
+def test_check_self_consistency(general_fs_tmpdir):
   write_some_file_in_tmpdir(general_fs_tmpdir)
   p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_file")
   with p.open() as f:
