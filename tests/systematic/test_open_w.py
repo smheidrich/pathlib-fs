f11b838cedfbd2eba4a18ccca385d231878f84e4
smheidrich <smheidrich@weltenfunktion.de>
2019-05-12 21:21:33 +0200

    Improved open(w) test in the same way

--- tests/systematic/test_open_w.py
+++ tests/systematic/test_open_w.py
@@ -7,24 +7,27 @@ def write_some_file_in_tmpdir(general_fs_tmpdir):
   p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_file")
   with p.open("w") as f:
     f.write("hello")
-
-def check_file_contents(f):
+  f = yield
   assert f.read() == "hello"
+  yield # dummy -> no StopIteration TODO
 
 def test_check_with_pyfilesystem(general_fs_tmpdir):
-  write_some_file_in_tmpdir(general_fs_tmpdir)
+  g = write_some_file_in_tmpdir(general_fs_tmpdir)
+  next(g)
   path = fs.path.join(general_fs_tmpdir.path, "some_file")
   with general_fs_tmpdir.fs.open(path) as f:
-    check_file_contents(f)
+    g.send(f)
 
 def test_check_with_pathlib(general_osfs_tmpdir):
-  write_some_file_in_tmpdir(general_osfs_tmpdir)
+  g = write_some_file_in_tmpdir(general_osfs_tmpdir)
+  next(g)
   path = Path(general_osfs_tmpdir.os_path)/"some_file"
   with path.open() as f:
-    check_file_contents(f)
+    g.send(f)
 
 def test_check_self_consistency(general_fs_tmpdir):
-  write_some_file_in_tmpdir(general_fs_tmpdir)
+  g = write_some_file_in_tmpdir(general_fs_tmpdir)
+  next(g)
   p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_file")
   with p.open() as f:
-    check_file_contents(f)
+    g.send(f)
