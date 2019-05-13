ec86cbd31115bdd57415d06b7827cc05f1808c17
smheidrich <smheidrich@weltenfunktion.de>
2019-05-13 03:16:46 +0200

    Rename

--- tests/systematic/test_open_r.py
+++ tests/systematic/test_open_r.py
@@ -14,19 +14,19 @@ def script(yield_f, general_fs_tmpdir):
   with p.open("rb") as f:
     assert f.read() == b"hello"
 
-def test_check_with_pyfilesystem(general_fs_tmpdir):
+def test_perform_with_pyfilesystem(general_fs_tmpdir):
   def yield_f():
     path = fs.path.join(general_fs_tmpdir.path, "some_file")
     return general_fs_tmpdir.fs.open(path, "w")
   script(yield_f, general_fs_tmpdir)
 
-def test_check_with_pathlib(general_osfs_tmpdir):
+def test_perform_with_pathlib(general_osfs_tmpdir):
   def yield_f():
     path = Path(general_osfs_tmpdir.os_path)/"some_file"
     return path.open("w")
   script(yield_f, general_osfs_tmpdir)
 
-def test_check_self_consistency(general_fs_tmpdir):
+def test_perform_self_consistency(general_fs_tmpdir):
   def yield_f():
     p = FsPath(general_fs_tmpdir.fs, general_fs_tmpdir.path, "some_file")
     return p.open("w")
