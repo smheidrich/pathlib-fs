c0495186f15af8fa049dc9049365a35f78d7d5eb
smheidrich <smheidrich@weltenfunktion.de>
2022-10-22 23:22:39 +0200

    Reduce setup.py to "dummy" (for editable inst.)

--- setup.py
+++ setup.py
@@ -1,32 +1,3 @@
-# coding: utf-8
 from setuptools import setup
 
-setup(
-  name="pathlib-fs",
-  # version is handled dynamically by setuptools_scm
-  use_scm_version = True,
-  description="Pathlib interface for PyFilesystem objects",
-  keywords="",
-  url="",
-  author="Shahriar Heidrich",
-  author_email="smheidrich@weltenfunktion.de",
-  classifiers=[
-    "Development Status :: 3 - Alpha",
-    "Environment :: Console",
-    "Operating System :: POSIX :: Linux",
-    "Programming Language :: Python :: 3.6",
-    "Programming Language :: Python :: 3.7",
-  ],
-  packages=["pathlib_fs"],
-  setup_requires=[
-    "pytest-runner",
-    "setuptools_scm",
-  ],
-  install_requires=[
-    "fs"
-  ],
-  tests_require=[
-    "pytest",
-    "testinfra",
-  ],
-)
+setup()
